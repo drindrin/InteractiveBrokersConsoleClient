99f3a00b456c697aaa4f553af39293fa59c786fb
Ed Mays <copperstarsystems@gmail.com>
2016-12-15 10:34:07 -0500

    Clean up code and add basic implementation to request current time upon connection.

--- Program.cs
+++ Program.cs
@@ -3,17 +3,71 @@
 // 2016/12/12
 //  --------------------------------------------------------------------------------------
 
+using System;
 using InteractiveBrokersConsoleClient.TwsApi;
 
 namespace InteractiveBrokersConsoleClient
 {
     class Program
     {
+        static TwsClient client;
+
+        static void ConnectToTws()
+        {
+            client.ClientSocket.eConnect("127.0.0.1", 7496, 0, false);
+        }
+
+        static void DisconnectFromClient()
+        {
+            // Disconnect from the client once we're done
+            Console.WriteLine("Disconnecting...");
+            client.ClientSocket.eDisconnect();
+        }
+
+        static void GetCurrentTime()
+        {
+            client.ClientSocket.reqCurrentTime();
+        }
+
+        // Application entry point
         static void Main(string[] args)
         {
-            var client = new TwsClient();
-            client.ClientSocket.eConnect("", 0, 0);
-            client.ClientSocket.startApi();
+            client = new TwsClient();
+
+            Console.WriteLine("Connecting...");
+            // Connect to the Trader Workstation client
+            ConnectToTws();
+            WaitForConnection();
+
+            // Now we can do whatever we want
+            Console.WriteLine("Please enter a ticker symbol");
+            GetCurrentTime();
+            // We will implement ticker feed functionality once we
+            // have the basic application working.
+            //
+            // var symbol = Console.ReadLine();
+            // SubscribeToTickerFeed(symbol);
+
+            DisconnectFromClient();
+            Console.WriteLine("Finished.");
+        }
+
+        // TODO:  Implement this method
+        //static void SubscribeToTickerFeed(string symbol)
+        //{
+        //    client.ClientSocket.reqMarketDataType(2);
+        //}
+
+        static void WaitForConnection()
+        {
+            // [From IBSamples.Program.cs]
+            /*************************************************************************************************************************************************/
+            /* One good way of knowing if we can proceed is by monitoring the order's nextValidId reception which comes down automatically after connecting. */
+            /*************************************************************************************************************************************************/
+            while (client.NextOrderId <= 0)
+            {
+                // Do nothing, we're waiting for a connection to happen.
+            }
         }
     }
 }
